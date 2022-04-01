0487d5424d457573497ed20810a16ea3767b590f
alissa-tung <alissa-tung@outlook.com>
2022-04-01 17:03:37 +0800

    *: basic framework

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 hstreamdb_erlang
 =====
 
-An OTP library
+An OTP library for HStreamDB
 
 Build
 -----
 
     $ rebar3 compile
+
+
+Example usage:
+
+```erl
+readme() ->
+    hstreamdb_erlang:start(normal, []),
+
+    hstreamdb_erlang:start_client_channel(ch, "http://127.0.0.1:6570"),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ch)]),
+
+    io:format("~p~n", [hstreamdb_erlang:delete_stream(ch, "test_stream")]),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ch)]),
+
+    io:format("~p~n", [hstreamdb_erlang:create_stream(ch, "test_stream", 3, 14)]),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ch)]),
+
+    XS = lists:seq(0, 100),
+    lists:foreach(
+        fun(X) ->
+            io:format("~p: ~p~n", [
+                X,
+                hstreamdb_erlang:append(
+                    ch,
+                    "test_stream",
+                    "",
+                    raw,
+                    <<"this_is_a_binary_literal">>
+                )
+            ])
+        end,
+        XS
+    ),
+    ok.
+```
--- src/hstreamdb_erlang.erl
+++ src/hstreamdb_erlang.erl
@@ -6,12 +6,15 @@
 -export([start/2, stop/1]).
 -export([init/1]).
 
--export([start_client_channel/2, start_client_channel/3]).
+-export([start_client_channel/2, start_client_channel/3, stop_client_channel/1]).
 -export([server_node_to_host_port/1]).
 
 -export([list_streams/1, create_stream/4, lookup_stream/3]).
+-export([append/5]).
 
 -export([list_subscriptions/1]).
+
+-export([readme/0]).
 
 %%--------------------------------------------------------------------
 %% APIs for application
@@ -42,15 +45,20 @@ start_client_channel(ChName, URL) ->
 start_client_channel(ChName, URL, Opts) ->
     hstreamdb_erlang_client:create_channel_pool(ChName, URL, Opts).
 
+stop_client_channel(ChName) -> hstreamdb_erlang_client:stop_channel_pool(ChName).
+
 %%--------------------------------------------------------------------
 
 list_streams(Ch) ->
-    Ret = hstream_server_h_stream_api_client:list_streams(#{}, #{
-        channel => Ch
-    }),
+    Ret = hstream_server_h_stream_api_client:list_streams(
+        #{},
+        #{channel => Ch}
+    ),
     case Ret of
-        {ok, Resp, _} -> {ok, maps:get(streams, Resp)};
-        R -> R
+        {ok, Resp, _} ->
+            {ok, maps:get(streams, Resp)};
+        R ->
+            R
     end.
 
 create_stream(Ch, StreamName, ReplicationFactor, BacklogDuration) ->
@@ -76,8 +84,10 @@ lookup_stream(Ch, StreamName, OrderingKey) ->
         #{channel => Ch}
     ),
     case Ret of
-        {ok, Resp, _} -> {ok, maps:get(serverNode, Resp)};
-        R -> R
+        {ok, Resp, _} ->
+            {ok, maps:get(serverNode, Resp)};
+        R ->
+            R
     end.
 
 string_format(Pattern, Values) ->
@@ -88,13 +98,105 @@ server_node_to_host_port(ServerNode) ->
     Port = maps:get(port, ServerNode),
     string_format("~s:~p", [Host, Port]).
 
+server_node_to_host_port(ServerNode, Protocol) ->
+    Host = maps:get(host, ServerNode),
+    Port = maps:get(port, ServerNode),
+    string_format("~p://~s:~p", [Protocol, Host, Port]).
+
+%%--------------------------------------------------------------------
+
+append(Ch, StreamName, OrderingKey, PayloadType, Payload) ->
+    {ok, ServerNode} = lookup_stream(Ch, StreamName, OrderingKey),
+    ServerUrl = server_node_to_host_port(ServerNode, http),
+    logger:notice(
+        "hstreamdb_erlang:append: lookup_stream_server_url = ~s",
+        [ServerUrl]
+    ),
+    Flag =
+        case PayloadType of
+            json -> 0;
+            raw -> 1
+        end,
+    Timestamp = #{
+        seconds => erlang:system_time(second),
+        nanos => erlang:system_time(nanosecond)
+    },
+    Header = #{
+        flag => Flag,
+        publish_time => Timestamp,
+        key => OrderingKey
+    },
+    Record = #{
+        header => Header,
+        payload => Payload
+    },
+    io:format("~p~n", [start_client_channel(innternal_append_ch, ServerUrl)]),
+
+    Req = #{
+        streamName => StreamName,
+        records => [Record]
+    },
+    logger:notice(
+        "hstreamdb_erlang:append: request = ~p",
+        [Req]
+    ),
+    Ret = hstream_server_h_stream_api_client:append(
+        Req,
+        #{channel => innternal_append_ch}
+    ),
+
+    % stop_client_channel(innternal_append_ch),
+    case Ret of
+        {ok, Resp, _} ->
+            {ok, maps:get(recordIds, Resp)};
+        R ->
+            R
+    end.
+
 %%--------------------------------------------------------------------
 
 list_subscriptions(Ch) ->
-    Ret = hstream_server_h_stream_api_client:list_subscriptions(#{}, #{
-        channel => Ch
-    }),
+    Ret = hstream_server_h_stream_api_client:list_subscriptions(
+        #{},
+        #{channel => Ch}
+    ),
     case Ret of
-        {ok, Resp, _} -> {ok, maps:get(subscription, Resp)};
-        R -> R
+        {ok, Resp, _} ->
+            {ok, maps:get(subscription, Resp)};
+        R ->
+            R
     end.
+
+%%--------------------------------------------------------------------
+
+readme() ->
+    start(normal, []),
+
+    start_client_channel(ch, "http://127.0.0.1:6570"),
+    io:format("~p~n", [list_streams(ch)]),
+
+    % io:format("~p~n", [delete_stream(ch, "test_stream")]),
+    io:format("~p~n", [list_streams(ch)]),
+
+    io:format("~p~n", [create_stream(ch, "test_stream", 3, 14)]),
+    io:format("~p~n", [list_streams(ch)]),
+
+    io:format("~p~n", [lookup_stream(ch, "test_stream", "")]),
+
+    XS = lists:seq(0, 100),
+    lists:foreach(
+        fun(X) ->
+            io:format("~p: ~p~n", [
+                X,
+                append(
+                    ch,
+                    "test_stream",
+                    "",
+                    raw,
+                    <<"this_is_a_binary_literal">>
+                )
+            ])
+        end,
+        XS
+    ),
+    ok.
