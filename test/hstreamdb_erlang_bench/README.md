89b7fbf4687282bd5040d5ad4fb525f36ad1f909
alissa-tung <alissa-tung@outlook.com>
2022-04-07 11:51:43 +0800

    *: init bench escript

---
+++ test/hstreamdb_erlang_bench/README.md
@@ -0,0 +1,14 @@
+hstreamdb_erlang_bench
+=====
+
+An escript
+
+Build
+-----
+
+    $ rebar3 escriptize
+
+Run
+---
+
+    $ _build/default/bin/hstreamdb_erlang_bench
---
+++ test/hstreamdb_erlang_bench/src/hstreamdb_erlang_bench.erl
@@ -0,0 +1,90 @@
+-module(hstreamdb_erlang_bench).
+
+%% API exports
+-export([main/1]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+%% escript Entry point
+main(Args) ->
+    io:format("Args: ~p~n", [Args]),
+
+    _ =
+        case Args of
+            ["readme"] ->
+                readme();
+            _ ->
+                ok
+        end,
+
+    erlang:halt(0).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+string_format(Pattern, Values) ->
+    lists:flatten(io_lib:format(Pattern, Values)).
+
+readme() ->
+    StreamName = string_format("test_stream-~p", [erlang:system_time(second)]),
+    ChannelName = string_format("test_channel-~p", [erlang:system_time(second)]),
+
+    hstreamdb_erlang:start(normal, []),
+
+    io:format("~p~n", [hstreamdb_erlang:start_client_channel(ChannelName, "http://127.0.0.1:6570")]),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ChannelName)]),
+
+    io:format("~p~n", [hstreamdb_erlang:create_stream(ChannelName, StreamName, 3, 14)]),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ChannelName)]),
+
+    XS = lists:seq(0, 100),
+    lists:foreach(
+        fun(X) ->
+            io:format("~p: ~p~n", [
+                X,
+                hstreamdb_erlang:append(
+                    ChannelName,
+                    StreamName,
+                    "",
+                    raw,
+                    <<"this_is_a_binary_literal">>
+                )
+            ])
+        end,
+        XS
+    ),
+    lists:foreach(
+        fun(X) ->
+            io:format("~p: ~p~n", [
+                X,
+                hstreamdb_erlang:append(
+                    ChannelName,
+                    StreamName,
+                    "",
+                    raw,
+                    lists:duplicate(10, <<"this_is_a_binary_literal">>)
+                )
+            ])
+        end,
+        XS
+    ),
+
+    io:format("~p~n", [hstreamdb_erlang:delete_stream(ChannelName, StreamName)]),
+    io:format("~p~n", [hstreamdb_erlang:list_streams(ChannelName)]),
+
+    io:format("~p~n", [hstreamdb_erlang:stop_client_channel(ChannelName)]),
+
+    ok.
+
+bit_size_128() -> (<<"___hstream.io___">>).
+
+get_bytes(Size, Unit) ->
+    SizeBytes =
+        case Unit of
+            k -> Size * 1024;
+            m -> Size * 1024 * 1024
+        end,
+    lists:replicate(SizeBytes / 128, bit_size_128()).
