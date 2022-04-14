604b0852802c9fb21e54b0c29a3efb97d3a62025
Alissa Tung <alissa@mail.ncut.edu.cn>
2022-04-14 14:35:14 +0800

    producer: merge v2

--- src/hstreamdb_erlang_producer.erl
+++ src/hstreamdb_erlang_producer.erl
@@ -1,274 +1,293 @@
 -module(hstreamdb_erlang_producer).
 
--behaviour(gen_statem).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2]).
 
--export([callback_mode/0]).
--export([start/0, start/1, start_link/0, start_link/1, init/1]).
-
--export([wait_for_append/3, appending/3]).
+-export([start/1, start_link/1]).
 
 -export([readme/0]).
 
-callback_mode() ->
-    % Events are handled by one callback function per state.
-    state_functions.
-
-start_link() ->
-    start_link([]).
-
-start_link(Args) ->
-    gen_statem:start_link(
-        ?MODULE,
-        Args,
-        []
-    ).
-
-start() ->
-    start([]).
-
-start(Args) ->
-    gen_statem:start(
-        ?MODULE,
-        Args,
-        []
-    ).
+% --------------------------------------------------------------------------------
 
 init(
     #{
-        producerOptions := ProducerOptions,
-        serverUrl := ServerUrl,
-        streamName := StreamName
-    } = Opts
+        producer_option := ProducerOption
+    } = _Args
 ) ->
-    init(ProducerOptions, ServerUrl, StreamName).
+    ProducerStatus = neutral_producer_status(),
+    State = build_producer_state(ProducerStatus, ProducerOption),
+    {ok, State}.
 
-init(ProducerOptions, ServerUrl, StreamName) ->
-    SelfPid = self(),
-    WorkerPid = spawn(
-        fun() ->
-            receive
-                flush ->
-                    gen_statem:call(
-                        SelfPid, {append, #{record => [], flush => true}}
-                    )
-            end
-        end
-    ),
-    timer:send_interval(5000, WorkerPid, flush),
+% --------------------------------------------------------------------------------
 
-    {ok, wait_for_append, #{
-        recordBuffer =>
-            #{
-                batchInfo => #{
-                    recordCount => 0,
-                    bytes => 0
-                },
-                records => []
-            },
-        options => ProducerOptions,
-        serverUrl => ServerUrl,
-        streamName => StreamName
-    }}.
-
-%%--------------------------------------------------------------------
-
-wait_for_append(
-    {call, From},
-    {_, EventContentMap} = EventContent,
+start(
     #{
-        recordBuffer := RecordBuffer,
-        options := ProducerOptions,
-        serverUrl := ServerUrl,
-        streamName := StreamName
-    } = Data
+        producer_option := _ProducerOption
+    } = Args
 ) ->
-    % logger:notice(#{
-    %     msg => "producer: do wait_for_append",
-    %     val => [EventContent, RecordBuffer]
-    % }),
+    gen_server:start(?MODULE, Args, []).
 
-    Record = maps:get(record, EventContentMap),
-    ProcessedBuffer = add_record_to_buffer(Record, RecordBuffer),
-    ProcessedData = maps:update(
-        recordBuffer, ProcessedBuffer, Data
-    ),
+start_link(
+    #{
+        producer_option := _ProducerOption
+    } = Args
+) ->
+    gen_server:start_link(?MODULE, Args, []).
 
-    DoFlush = maps:get(doFlush, EventContentMap, false),
+% --------------------------------------------------------------------------------
 
-    gen_statem:reply(From, ok),
-    case DoFlush orelse check_ready_for_append(ProcessedBuffer, ProducerOptions) of
-        true ->
-            Records = maps:get(records, ProcessedBuffer),
-            io:format("~n~p~n", [exec_append(ServerUrl, Records, StreamName)]),
+build_batch_setting(BatchSetting) when is_tuple(BatchSetting) ->
+    {BatchSettingAtom, BatchSettingValue} = BatchSetting,
+    BatchSettingMap = #{BatchSettingAtom => BatchSettingValue},
+    build_batch_setting(BatchSettingMap);
+build_batch_setting(BatchSetting) when is_list(BatchSetting) ->
+    BatchSettingMap = maps:from_list(BatchSetting),
+    build_batch_setting(BatchSettingMap);
+build_batch_setting(BatchSetting) when is_map(BatchSetting) ->
+    Get = fun(X) -> maps:get(X, BatchSetting, undefined) end,
+    RecordCountLimit = Get(record_count_limit),
+    BytesLimit = Get(bytes_limit),
+    AgeLimit = Get(age_limit),
+    build_batch_setting(RecordCountLimit, BytesLimit, AgeLimit).
 
-            {next_state, wait_for_append, maps:update(recordBuffer, empty_buffer(), Data)};
-        false ->
-            {next_state, wait_for_append, ProcessedData}
+check_batch_setting(RecordCountLimit, BytesLimit, AgeLimit) ->
+    BatchSettingList = [RecordCountLimit, BytesLimit, AgeLimit],
+    try
+        true = lists:any(fun(X) -> X /= undefined end, BatchSettingList)
+    catch
+        error:_ ->
+            throw(hstreamdb_exception)
     end.
 
-appending(
-    {call, From},
-    EventContent,
+build_batch_setting(RecordCountLimit, BytesLimit, AgeLimit) ->
+    check_batch_setting(RecordCountLimit, BytesLimit, AgeLimit),
     #{
-        recordBuffer := RecordBuffer,
-        options := ProducerOptions
-    } = Data
-) ->
-    % logger:notice(#{
-    %     msg => "producer: do appending",
-    %     val => EventContent
-    % }),
-
-    ProcessedBatchInfo = #{
-        recordCount => 0,
-        byte => 0
-    },
-    ProcessedBuffer = #{
-        batchInfo => ProcessedBatchInfo,
-        records => []
-    },
-    ProcessedData = maps:update(
-        recordBuffer, ProcessedBuffer, Data
-    ),
-
-    gen_statem:reply(From, ok),
-    {next_state, wait_for_append, ProcessedData}.
-
-%%--------------------------------------------------------------------
-
-check_ready_for_append(
-    #{
-        batchInfo := #{
-            recordCount := RecordCount,
-            bytes := Bytes
-        }
-    } = ProducerInfo,
-    #{
-        batchSetting := BatchSetting
-    } = ProducerOptions
-) ->
-    % logger:notice(#{
-    %     msg => "producer: check_ready_for_append",
-    %     val => [ProducerInfo, ProducerOptions]
-    % }),
-
-    [
-        RecordCountLimit,
-        BytesLimit
-    ] =
-        BatchSettings = lists:map(
-            fun(X) ->
-                maps:get(X, BatchSetting, undefined)
-            end,
-            [
-                recordCountLimit,
-                bytesLimit
-            ]
-        ),
-    true = lists:any(fun(X) -> X =/= undefined end, BatchSettings),
-
-    lists:any(
-        fun({X, XLimit}) ->
-            case XLimit of
-                undefined -> false;
-                Limit when is_integer(Limit) -> X >= Limit
-            end
-        end,
-        [
-            {RecordCount, RecordCountLimit},
-            {Bytes, BytesLimit}
-        ]
-    ).
-
-add_record_to_buffer(
-    Record,
-    #{
-        batchInfo := BatchInfo,
-        records := _
-    } = Buffer
-) ->
-    BatchInfo0 = maps:update_with(
-        recordCount, fun(X) -> X + 1 end, BatchInfo
-    ),
-    BatchInfo1 = maps:update_with(
-        bytes, fun(X) -> X + byte_size(Record) end, BatchInfo0
-    ),
-    Buffer0 = maps:update(
-        batchInfo, BatchInfo1, Buffer
-    ),
-    Buffer1 = maps:update_with(
-        records, fun(XS) -> [Record | XS] end, Buffer0
-    ),
-
-    % logger:notice(#{
-    %     msg => "producer: do add_record_to_buffer",
-    %     val => [Buffer, Buffer1]
-    % }),
-
-    Buffer1.
-
-empty_buffer() ->
-    #{
-        batchInfo => #{
-            recordCount => 0,
-            bytes => 0
-        },
-        records => []
+        record_count_limit => RecordCountLimit,
+        bytes_limit => BytesLimit,
+        age_limit => AgeLimit
     }.
 
-exec_append(
-    ServerUrl, Records, StreamName
+build_producer_option(ServerUrl, StreamName, BatchSetting) ->
+    #{
+        server_url => ServerUrl,
+        stream_name => StreamName,
+        batch_setting => BatchSetting
+    }.
+
+build_batch_status(RecordCount, Bytes) ->
+    #{
+        record_count => RecordCount,
+        bytes => Bytes
+    }.
+
+build_producer_status(Records, BatchStatus) ->
+    #{
+        records => Records,
+        batch_status => BatchStatus
+    }.
+
+build_producer_state(ProducerStatus, ProducerOption) ->
+    #{
+        producer_status => ProducerStatus,
+        producer_option => ProducerOption
+    }.
+
+neutral_batch_status() ->
+    build_batch_status(0, 0).
+
+neutral_producer_status() ->
+    build_producer_status([], neutral_batch_status()).
+
+% --------------------------------------------------------------------------------
+
+add_to_buffer(
+    Record,
+    #{
+        producer_status := ProducerStatus,
+        producer_option := ProducerOption
+    } = _State
+) when is_binary(Record) ->
+    #{
+        records := Records,
+        batch_status := BatchStatus
+    } = ProducerStatus,
+    #{
+        record_count := RecordCount,
+        bytes := Bytes
+    } = BatchStatus,
+
+    NewRecords = [Record | Records],
+    NewRecordCount = RecordCount + 1,
+    NewBytes = Bytes + byte_size(Record),
+    NewBatchStatus = build_batch_status(NewRecordCount, NewBytes),
+
+    NewProducerStatus = build_producer_status(NewRecords, NewBatchStatus),
+    build_producer_state(NewProducerStatus, ProducerOption).
+
+check_buffer_limit(
+    #{
+        producer_status := ProducerStatus,
+        producer_option := ProducerOption
+    } = _State
 ) ->
-    {ok, Ch} = hstreamdb_erlang:start_client_channel(ServerUrl),
+    #{
+        batch_status := #{
+            record_count := RecordCount,
+            bytes := Bytes
+        }
+    } = ProducerStatus,
+    #{
+        batch_setting := #{
+            record_count_limit := RecordCountLimit,
+            bytes_limit := BytesLimit
+        }
+    } = ProducerOption,
+
+    CheckLimit = fun({X, XS}) ->
+        case XS of
+            undefined -> false;
+            XLimit when is_integer(XLimit) -> X >= XLimit
+        end
+    end,
+
+    CheckLimit({RecordCount, RecordCountLimit}) orelse
+        CheckLimit({Bytes, BytesLimit}).
+
+clear_buffer(
+    #{
+        producer_option := ProducerOption
+    } = _State
+) ->
+    ProducerStatus = neutral_producer_status(),
+    build_producer_state(ProducerStatus, ProducerOption).
+
+% --------------------------------------------------------------------------------
+
+handle_call({Method, Body} = Request, From, State) ->
+    case Method of
+        flush -> exec_flush(Body, State);
+        append -> exec_append(Body, State);
+        _ -> throw(hstreamdb_exception)
+    end.
+
+handle_cast(_, _) ->
+    throw(hstreamdb_exception).
+
+% --------------------------------------------------------------------------------
+
+build_append_request(Record) ->
+    {append, #{
+        record => Record
+    }}.
+
+build_flush_request() ->
+    {flush, #{}}.
+
+% --------------------------------------------------------------------------------
+
+append(Records, ServerUrl, StreamName) ->
+    {ok, Channel} = hstreamdb_erlang:start_client_channel(ServerUrl),
     OrderingKey = "",
     PayloadType = raw,
-    Payload = Records,
-
-    Key = rpc:async_call(node(), hstreamdb_erlang, append, [
-        Ch, StreamName, OrderingKey, PayloadType, Payload
-    ]),
-    {ok, Ret} = rpc:yield(Key),
-    hstreamdb_erlang:stop_client_channel(Ch),
+    Ret =
+        hstreamdb_erlang:append(
+            Channel, StreamName, OrderingKey, PayloadType, Records
+        ),
+    _ = hstreamdb_erlang:stop_client_channel(Channel),
     Ret.
 
-%%--------------------------------------------------------------------
+exec_flush(
+    _FlushRequest,
+    #{
+        producer_status := ProducerStatus,
+        producer_option := ProducerOption
+    } = State
+) ->
+    #{
+        records := Records
+    } = ProducerStatus,
+    #{
+        server_url := ServerUrl,
+        stream_name := StreamName
+    } = ProducerOption,
+
+    Reply = append(Records, ServerUrl, StreamName),
+
+    NewState = clear_buffer(State),
+    {reply, Reply, NewState}.
+
+exec_append(
+    #{
+        record := Record
+    } = _AppendRequest,
+    State
+) ->
+    State0 = add_to_buffer(Record, State),
+    case check_buffer_limit(State0) of
+        true ->
+            FlushRequest = build_flush_request(),
+            exec_flush(FlushRequest, State0);
+        false ->
+            Reply = ok,
+            NewState = State0,
+            {reply, Reply, NewState}
+    end.
+
+% --------------------------------------------------------------------------------
 
 readme() ->
-    {ok, Pid} = start_link(#{
-        streamName => "xxxx",
-        serverUrl => "http://127.0.0.1:6570",
-        producerOptions => #{
-            batchSetting => #{
-                recordCountLimit => 3
-            }
-        }
+    ServerUrl = "http://127.0.0.1:6570",
+    StreamName = "___v2_test___",
+    BatchSetting = build_batch_setting({record_count_limit, 3}),
+
+    % {ok, _} = hstreamdb_erlang:start(normal, []),
+    {ok, Channel} = hstreamdb_erlang:start_client_channel(ServerUrl),
+    _ = hstreamdb_erlang:delete_stream(Channel, StreamName, #{
+        ignoreNonExist => true,
+        force => true
     }),
+    ReplicationFactor = 3,
+    BacklogDuration = 60 * 30,
+    ok = hstreamdb_erlang:create_stream(
+        Channel, StreamName, ReplicationFactor, BacklogDuration
+    ),
+    _ = hstreamdb_erlang:stop_client_channel(Channel),
 
-    % {ok, Ch} = hstreamdb_erlang:start_client_channel("http://127.0.0.1:6570"),
-    % hstreamdb_erlang:create_stream(Ch, "xxxx", 3, 15),
+    StartArgs = #{
+        producer_option => build_producer_option(ServerUrl, StreamName, BatchSetting)
+    },
+    {ok, ProducerPid} = start_link(StartArgs),
 
-    gen_statem:call(
-        Pid, {append, #{record => <<"00">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"01">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"02">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"03">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"04">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"05">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"06">>}}
-    ),
-    gen_statem:call(
-        Pid, {append, #{record => <<"07">>}}
-    ),
+    io:format("StartArgs: ~p~n", [StartArgs]),
+    io:format("~p~n", [
+        gen_server:call(
+            ProducerPid,
+            build_append_request(<<"00">>)
+        )
+    ]),
+    io:format("~p~n", [
+        gen_server:call(
+            ProducerPid,
+            build_append_request(<<"01">>)
+        )
+    ]),
+    io:format("~p~n", [
+        gen_server:call(
+            ProducerPid,
+            build_append_request(<<"02">>)
+        )
+    ]),
+    io:format("~p~n", [
+        gen_server:call(
+            ProducerPid,
+            build_append_request(<<"03">>)
+        )
+    ]),
+    io:format("~p~n", [
+        gen_server:call(
+            ProducerPid,
+            build_append_request(<<"04">>)
+        )
+    ]),
+
     ok.
