85106ef6b200a015a425004275232c19a5eeb07a
Alissa Tung <alissa@mail.ncut.edu.cn>
2022-04-16 01:04:20 +0800

    refactor

--- src/hstreamdb_erlang_producer.erl
+++ src/hstreamdb_erlang_producer.erl
@@ -10,8 +10,6 @@
     build_record/1, build_record/2, build_record/3
 ]).
 
--export([readme/0]).
-
 % --------------------------------------------------------------------------------
 
 init(
@@ -396,53 +394,3 @@ exec_append(
             NewState = State0,
             {reply, Reply, NewState}
     end.
-
-% --------------------------------------------------------------------------------
-
-readme() ->
-    ServerUrl = "http://127.0.0.1:6570",
-    StreamName = hstreamdb_erlang_utils:string_format("~s-~p", [
-        "___v2_test___", erlang:time()
-    ]),
-    BatchSetting = build_batch_setting({record_count_limit, 3}),
-
-    {ok, Channel} = hstreamdb_erlang:start_client_channel(ServerUrl),
-    _ = hstreamdb_erlang:delete_stream(Channel, StreamName, #{
-        ignoreNonExist => true,
-        force => true
-    }),
-    ReplicationFactor = 3,
-    BacklogDuration = 60 * 30,
-    ok = hstreamdb_erlang:create_stream(
-        Channel, StreamName, ReplicationFactor, BacklogDuration
-    ),
-    _ = hstreamdb_erlang:stop_client_channel(Channel),
-
-    StartArgs = #{
-        producer_option => build_producer_option(ServerUrl, StreamName, BatchSetting)
-    },
-    {ok, Producer} = start_link(StartArgs),
-
-    io:format("StartArgs: ~p~n", [StartArgs]),
-
-    RecordIds = lists:map(
-        fun(_) ->
-            Record = build_record(<<"_">>),
-            append(Producer, Record)
-        end,
-        lists:seq(0, 100)
-    ),
-
-    flush(Producer),
-
-    timer:sleep(1000),
-
-    lists:foreach(
-        fun({ok, FutureRecordId}) ->
-            RecordId = rpc:yield(FutureRecordId),
-            io:format("~p~n", [RecordId])
-        end,
-        RecordIds
-    ),
-
-    ok.
