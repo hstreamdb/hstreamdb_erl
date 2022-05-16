0b9d99f481a127d8b179a6b66701e5cd691660b7
alissa-tung <alissa-tung@outlook.com>
2022-05-16 17:28:54 +0800

    chore: rm unused mod

--- fix.sh
+++ fix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-rebar3 grpc gen && find . -type f -name '*.erl' -exec sed -i "s/HStreamApi:/'HStreamApi':/g" {} \;
+rebar3 grpc gen && rm -rf ./src/hstream_server_h_stream_api_bhvr.erl && find . -type f -name '*.erl' -exec sed -i "s/HStreamApi:/'HStreamApi':/g" {} \;
