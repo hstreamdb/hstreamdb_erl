d7e55ab32df3924e95cb7ad3838543fb707f5a0a
alissa-tung <alissa-tung@outlook.com>
2022-08-24 13:17:25 +0800

    bench: init

---
+++ src/cnt.erl
@@ -0,0 +1,23 @@
+-module(cnt).
+
+-behaviour(gen_server).
+
+-export([init/1, handle_info/2, handle_call/3, handle_cast/2]).
+
+init([X]) ->
+  io:format("start time: ~p~n", [erlang:system_time()]),
+  Timer = erlang:send_after(1, self(), check),
+  {ok, {Timer, 0, X}}.
+
+handle_info(check, {OldTimer, Cnt, X}) ->
+  erlang:cancel_timer(OldTimer),
+  io:format("~p ~p~n", [atomics:get(X, 1), Cnt]),
+  atomics:put(X, 1, 0),
+  Timer = erlang:send_after(10000, self(), check),
+  {noreply, {Timer, Cnt + 1, X}}.
+
+handle_call(_R, _F, S) ->
+  {reply, ok, S}.
+
+handle_cast(R, S) ->
+  handle_info(R, S).
--- src/hstreamdb_appender.erl
+++ src/hstreamdb_appender.erl
@@ -5,20 +5,23 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
--record(state, {channel_manager}).
+-record(state, {channel_manager, byte_size_ref}).
 
 init(Options) ->
-  {ok, #state{channel_manager = hstreamdb_channel_mgr:start(Options)}}.
+  ByteSizeRef = proplists:get_value(byte_size_ref, Options, undefined),
+  {ok,
+   #state{channel_manager = hstreamdb_channel_mgr:start(Options),
+          byte_size_ref = ByteSizeRef}}.
 
 handle_call(Request, _From, State) ->
   {noreply, NState} = handle_cast(Request, State),
   {reply, ok, NState}.
 
 handle_cast({append, {Stream, OrderingKey, Records}},
-            State = #state{channel_manager = ChannelM}) ->
+            State = #state{channel_manager = ChannelM, byte_size_ref = ByteSizeRef}) ->
   case hstreamdb_channel_mgr:lookup_channel(OrderingKey, ChannelM) of
     {ok, Channel} ->
-      case call_rpc_append(Stream, Records, Channel) of
+      case call_rpc_append(Stream, Records, Channel, ByteSizeRef) of
         {ok, _} ->
           {noreply, State};
         {error, _} ->
@@ -26,7 +29,7 @@ handle_cast({append, {Stream, OrderingKey, Records}},
           {noreply, State#state{channel_manager = NChannelM}}
       end;
     {ok, Channel, NChannelM} ->
-      case call_rpc_append(Stream, Records, Channel) of
+      case call_rpc_append(Stream, Records, Channel, ByteSizeRef) of
         {ok, _} ->
           {noreply, State#state{channel_manager = NChannelM}};
         {error, _} ->
@@ -51,11 +54,20 @@ terminate(_Reason, #state{channel_manager = ChannelM}) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-call_rpc_append(Stream, Records, Channel) ->
+call_rpc_append(Stream, Records, Channel, ByteSizeRef) ->
   Request = #{streamName => Stream, records => Records},
-  Options = Options = #{channel => Channel},
+  Options = #{channel => Channel},
   case hstreamdb_client:append(Request, Options) of
     {ok, AppendResponse, _MetaData} ->
+      Size =
+        lists:foldl(fun(X, Sum) ->
+                       erlang:byte_size(
+                         maps:get(payload, X))
+                       + Sum
+                    end,
+                    0,
+                    Records),
+      atomics:add(ByteSizeRef, 1, Size),
       {ok, AppendResponse};
     {error, AppendErrorReason} ->
       {error, AppendErrorReason}
